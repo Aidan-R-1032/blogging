9cdb2ea7b7fd56cac5a20c22332cf38cb2183fb9
Aidan Ruiz <agruiz6742@gmail.com>
2025-10-22 21:02:24 -0500

    started working on a delete post button

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,3 +1,6 @@
+import DeleteWarningButton from "./DeleteWarningButton"
+import { useState, useEffect } from "react"
+
 const div_styles = {
     'backgroundColor': 'lightsalmon',
     'display': 'flex',
@@ -16,12 +19,61 @@ const p_styles = {
     'padding': '0.5rem'
 }
 
+const deleteTogglerStyle = {
+  'display' : 'flex',
+  'justifyContent' : 'center',
+  'alignItems' : 'center',
+  'width' : '2rem',              
+  'height' : '2rem',
+  'backgroundColor' : 'lightsalmon',
+  'color' : 'white',
+  'border' : '1px solid white',
+  'borderRadius' : '8px',
+  'cursor' : 'pointer',
+  'fontWeight' : 'bold',
+  'fontSize' : '1rem',
+  'margin' : '0.5rem 0.5rem auto 0.3rem'
+};
+
+
 
 function Post(props){
+    const [displayDelete, setDisplayDelete] = useState(false);
+
+    const toggleDelete = () => {
+        setDisplayDelete(!displayDelete);
+    }
+
+    useEffect(() => {   // prevents scrolling
+        if(displayDelete) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'auto';
+        }
+
+        return () => {
+            document.body.style.overflow = "auto";
+        };
+    }, [displayDelete]);
     return (
         <div style={div_styles}>
-            <h1 style={h1_styles}> {props.title} </h1>
-            <p style={p_styles}> {props.body} </p>
+            <div style={{'marginLeft' : 'auto'}}>
+                <button 
+                    onClick={toggleDelete}
+                    style={deleteTogglerStyle}>
+                        X 
+                </button>
+            </div>
+            {displayDelete && <DeleteWarningButton 
+                alertHead={"Are you sure you want to delete this post?"}
+                alertMsg={`Once you delete a post, you will not be able to view it or any comments attactched. 
+                    Please make sure you are certain!`}
+                deleteFunc={() => {console.log(`Deleted post titled ${props.title}`)}}
+            />}
+            {!displayDelete && <div>
+                <h1 style={h1_styles}> {props.title} </h1>
+                <p style={p_styles}> {props.body} </p>
+            </div>}
         </div>
     )
 }
